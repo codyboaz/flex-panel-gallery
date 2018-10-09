81e849c7ff18a3495e0e4abd4a53a8b31733037a
Cody Boaz <codyryanboaz@yahoo.com>
2018-10-09 07:41:36 -0700

    interactive image gallery made with flexbox and vanilla js

---
+++ js/index.js
@@ -0,0 +1,14 @@
+const panels = document.querySelectorAll('.panel');
+
+function togglePanel() {
+    this.classList.toggle('open');
+}
+
+function toggleText(e) {
+    if(e.propertyName.includes('flex')) {
+        this.classList.toggle('open-active');
+    }
+}
+
+panels.forEach(panel => panel.addEventListener('click', togglePanel));
+panels.forEach(panel => panel.addEventListener('transitionend', toggleText));
